1d24a95ddf30746bc515effa6937b87d7a82f3a2
Ivan Phrolov <10litrov@gmail.com>
2015-04-11 17:28:05 +1000

    PyPa preparation.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='aiosterisk',
+    version='0.0.1',
+    packages=['aiosterisk'],
+    url='https://github.com/asterisk/starpy',
+    license='',
+    author='Ivan Phrolov',
+    author_email='10litrov@gmail.com',
+    description='Python AsyncIO library for Asterisk Manafer Interface'
+)
